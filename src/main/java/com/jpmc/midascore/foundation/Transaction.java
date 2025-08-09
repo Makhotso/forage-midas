6002f8b20d0eb6d0fc1d6cd3001e946df61a442f
Thato Mphahlele <thato.makhotso@gmail.com>
2025-08-09 21:23:07 +0200

    Create .gitattributes

--- src/main/java/com/jpmc/midascore/foundation/Transaction.java
+++ src/main/java/com/jpmc/midascore/foundation/Transaction.java
@@ -2,6 +2,7 @@ package com.jpmc.midascore.foundation;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Transaction {
     private long senderId;
