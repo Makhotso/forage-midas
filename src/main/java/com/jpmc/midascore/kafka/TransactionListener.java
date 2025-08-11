327b37559570209a38eb8399143bfb8e74c6dd04
Thato Mphahlele <thato.makhotso@gmail.com>
2025-08-11 15:28:47 +0200

    Final Commit

--- src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
@@ -1,17 +1,18 @@
 package com.jpmc.midascore.kafka;
 
-
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class TransactionListener {
 
-    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
-    public void listen(Transaction transaction){
-        System.out.println("Received Transaction" + transaction);
+    @KafkaListener(
+            topics = "${general.kafka-topic}",
+            groupId = "midas-core-group",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listen(Transaction transaction) {
+        System.out.println("✅ Received Transaction: " + transaction);
     }
 }
